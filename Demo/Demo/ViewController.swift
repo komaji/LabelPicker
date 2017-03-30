c15568ae0c4ab7e2cd25fa9b0e139c749277782c
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-03-30 23:07:34 +0900

    create demo project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by KojimaTatsuya on 2017/03/30.
+//  Copyright © 2017年 komaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

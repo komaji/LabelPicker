cc1ed591a467eba4ae346da30d856058643071ad
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-03-30 23:04:15 +0900

    git init

---
+++ LabelPicker/LabelPicker.h
@@ -0,0 +1,19 @@
+//
+//  LabelPicker.h
+//  LabelPicker
+//
+//  Created by KojimaTatsuya on 2017/03/30.
+//  Copyright © 2017年 komaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LabelPicker.
+FOUNDATION_EXPORT double LabelPickerVersionNumber;
+
+//! Project version string for LabelPicker.
+FOUNDATION_EXPORT const unsigned char LabelPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LabelPicker/PublicHeader.h>
+
+

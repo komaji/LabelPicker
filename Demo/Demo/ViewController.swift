4dd06463922af79bed9303c56e5b7a1bdb1ae343
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-03-31 23:20:39 +0900

    add demo for init PickerView in viewDidLoad

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -12,20 +12,24 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var pickerView: PickerView! {
         didSet {
-            pickerView.components = components
+            pickerView.components = timeComponents
             pickerView.rowHeight = 30.0
         }
     }
     
-    let components: [PickerComponent] = {
-        let items = (1...100).map { "\($0)" }
+    @IBOutlet weak var pickerFrameView: UIView!
+    
+    let timeComponents: [PickerComponent] = {
+        let hours = (1...24).map { "\($0)" }
+        let min = (0...60).map { "\($0)" }
+        let sec = (0...60).map { "\($0)" }
         let attributes = [
             NSFontAttributeName: UIFont.systemFont(ofSize: 16.0)
         ]
         
         return [
             PickerComponent(
-                items: items,
+                items: hours,
                 itemAttributes: attributes,
                 label: "hours",
                 labelAttributes: attributes,
@@ -33,7 +37,7 @@ class ViewController: UIViewController {
                 labelWidth: 50.0
             ),
             PickerComponent(
-                items: items,
+                items: min,
                 itemAttributes: attributes,
                 label: "min",
                 labelAttributes: attributes,
@@ -41,7 +45,7 @@ class ViewController: UIViewController {
                 labelWidth: 30.0
             ),
             PickerComponent(
-                items: items,
+                items: sec,
                 itemAttributes: attributes,
                 label: "sec",
                 labelAttributes: attributes,
@@ -50,6 +54,55 @@ class ViewController: UIViewController {
             ),
         ]
     }()
+    
+    let dateComponents: [PickerComponent] = {
+        let year = (1970...2017).map { "\($0)" }
+        let month = (1...12).map { "\($0)" }
+        let day = (1...31).map { "\($0)" }
+        let attributes = [
+            NSFontAttributeName: UIFont.systemFont(ofSize: 16.0)
+        ]
+        
+        return [
+            PickerComponent(
+                items: year,
+                itemAttributes: attributes,
+                label: "year",
+                labelAttributes: attributes,
+                maxItemWidth: 40.0,
+                labelWidth: 40.0,
+                widthRelativeRatio: 8.0
+            ),
+            PickerComponent(
+                items: month,
+                itemAttributes: attributes,
+                label: "month",
+                labelAttributes: attributes,
+                maxItemWidth: 20.0,
+                labelWidth: 50.0,
+                widthRelativeRatio: 7.0
+            ),
+            PickerComponent(
+                items: day,
+                itemAttributes: attributes,
+                label: "day",
+                labelAttributes: attributes,
+                maxItemWidth: 20.0,
+                labelWidth: 30.0,
+                widthRelativeRatio: 5.0
+            ),
+        ]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let pickerView = PickerView(frame: pickerFrameView.bounds)
+        pickerView.components = dateComponents
+        pickerView.rowHeight = 30.0
+        
+        pickerFrameView.addSubview(pickerView)
+    }
     
 }
 

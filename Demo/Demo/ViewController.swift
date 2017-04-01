962389782d0a348bf715824dc4b5a9ec8ceb4cbd
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-04-01 21:24:28 +0900

    update Demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var pickerView: PickerView! {
+    @IBOutlet weak var pickerView: LabelPickerView! {
         didSet {
             pickerView.components = timeComponents
             pickerView.rowHeight = 30.0
+            pickerView.contentSeparateWidth = 5.0
         }
     }
     
     @IBOutlet weak var pickerFrameView: UIView!
     
-    let timeComponents: [PickerComponent] = {
+    let timeComponents: [LabelPickerComponent] = {
         let hours = (1...24).map { "\($0)" }
         let min = (0...60).map { "\($0)" }
         let sec = (0...60).map { "\($0)" }
@@ -28,34 +29,34 @@ class ViewController: UIViewController {
         ]
         
         return [
-            PickerComponent(
+            LabelPickerComponent(
                 items: hours,
                 itemAttributes: attributes,
-                label: "hours",
+                maxItemWidth: 20.0,
+                labelName: "hours",
                 labelAttributes: attributes,
-                maxItemWidth: 30.0,
-                labelWidth: 50.0
+                labelNameWidth: 50.0
             ),
-            PickerComponent(
+            LabelPickerComponent(
                 items: min,
                 itemAttributes: attributes,
-                label: "min",
+                maxItemWidth: 20.0,
+                labelName: "min",
                 labelAttributes: attributes,
-                maxItemWidth: 30.0,
-                labelWidth: 30.0
+                labelNameWidth: 30.0
             ),
-            PickerComponent(
+            LabelPickerComponent(
                 items: sec,
                 itemAttributes: attributes,
-                label: "sec",
+                maxItemWidth: 20.0,
+                labelName: "sec",
                 labelAttributes: attributes,
-                maxItemWidth: 30.0,
-                labelWidth: 30.0
+                labelNameWidth: 30.0
             ),
         ]
     }()
     
-    let dateComponents: [PickerComponent] = {
+    let dateComponents: [LabelPickerComponent] = {
         let year = (1970...2017).map { "\($0)" }
         let month = (1...12).map { "\($0)" }
         let day = (1...31).map { "\($0)" }
@@ -64,32 +65,29 @@ class ViewController: UIViewController {
         ]
         
         return [
-            PickerComponent(
+            LabelPickerComponent(
                 items: year,
                 itemAttributes: attributes,
-                label: "year",
-                labelAttributes: attributes,
                 maxItemWidth: 40.0,
-                labelWidth: 40.0,
-                widthRelativeRatio: 8.0
+                labelName: "year",
+                labelAttributes: attributes,
+                labelNameWidth: 40.0
             ),
-            PickerComponent(
+            LabelPickerComponent(
                 items: month,
                 itemAttributes: attributes,
-                label: "month",
-                labelAttributes: attributes,
                 maxItemWidth: 20.0,
-                labelWidth: 50.0,
-                widthRelativeRatio: 7.0
+                labelName: "month",
+                labelAttributes: attributes,
+                labelNameWidth: 50.0
             ),
-            PickerComponent(
+            LabelPickerComponent(
                 items: day,
                 itemAttributes: attributes,
-                label: "day",
-                labelAttributes: attributes,
                 maxItemWidth: 20.0,
-                labelWidth: 30.0,
-                widthRelativeRatio: 5.0
+                labelName: "day",
+                labelAttributes: attributes,
+                labelNameWidth: 30.0
             ),
         ]
     }()
@@ -97,9 +95,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let pickerView = PickerView(frame: pickerFrameView.bounds)
+        let pickerView = LabelPickerView(frame: pickerFrameView.bounds)
         pickerView.components = dateComponents
         pickerView.rowHeight = 30.0
+        pickerView.componentsWidthEqual = false
         
         pickerFrameView.addSubview(pickerView)
     }

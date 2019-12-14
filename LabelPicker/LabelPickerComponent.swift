ef7f8763ec9c96f66668555a7a3fbd9cd86860d3
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2019-12-14 22:03:54 +0900

    Use memberwise initializer

--- LabelPicker/LabelPickerComponent.swift
+++ LabelPicker/LabelPickerComponent.swift
@@ -13,11 +13,6 @@ public struct LabelPickerComponent {
     public let items: ItemsComponent
     public let label: LabelComponent
     
-    public init(items: ItemsComponent, label: LabelComponent) {
-        self.items = items
-        self.label = label
-    }
-    
     public func attributedItem(index: Int) -> NSAttributedString {
         return NSAttributedString(string: items.items[index], attributes: items.attributes)
     }

4c79fa390a3837aed540318016f617046049c8a5
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2019-12-14 21:06:29 +0900

    Make demo easier to compare spacing

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var pickerFrameView1: UIView! {
         didSet {
             let pickerView = LabelPickerView(frame: pickerFrameView1.bounds)
-            pickerView.components = timeComponents
+            pickerView.components = dateComponents
             pickerView.rowHeight = 30.0
             
             pickerFrameView1.addSubview(pickerView)
@@ -30,43 +30,7 @@ class ViewController: UIViewController {
             pickerFrameView2.addSubview(pickerView)
         }
     }
-    
-    let timeComponents: [LabelPickerComponent] = {
-        let hours = (1...24).map { "\($0)" }
-        let min = (0...60).map { "\($0)" }
-        let sec = (0...60).map { "\($0)" }
-        let attributes: [NSAttributedStringKey: Any] = [
-            .font: UIFont.systemFont(ofSize: 16.0)
-        ]
-        
-        return [
-            LabelPickerComponent(
-                items: hours,
-                itemAttributes: attributes,
-                maxItemWidth: 20.0,
-                labelName: "hours",
-                labelAttributes: attributes,
-                labelNameWidth: 50.0
-            ),
-            LabelPickerComponent(
-                items: min,
-                itemAttributes: attributes,
-                maxItemWidth: 20.0,
-                labelName: "min",
-                labelAttributes: attributes,
-                labelNameWidth: 30.0
-            ),
-            LabelPickerComponent(
-                items: sec,
-                itemAttributes: attributes,
-                maxItemWidth: 20.0,
-                labelName: "sec",
-                labelAttributes: attributes,
-                labelNameWidth: 30.0
-            ),
-        ]
-    }()
-    
+
     let dateComponents: [LabelPickerComponent] = {
         let year = (1970...2017).map { "\($0)" }
         let month = (1...12).map { "\($0)" }
